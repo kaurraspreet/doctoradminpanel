21e7fccfb88cc38eb2aa4c976c2aa8af959b1e95
“Raspreet” <raspreet.dev@gmail.com>
2023-11-24 20:19:40 +0530

    update icon and keys

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -63,36 +63,34 @@ const Sidebar = () => {
       >
         <div className='h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'>
           <div className='flex flex-col space-y-1 font-medium'>
-            
-              {mainRoutes.map(({ name, href, icon }) => (
-                <Link
-                  href={href}
-                  className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
-                >
-                  {/* <SvgIcon
-                    name={icon}
-                    className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-white'
-                  /> */}
-                  <span className='ms-3'>{name}</span>
-                </Link>
-              ))}
-          
+            {mainRoutes.map(({ name, href, icon }) => (
+              <Link
+                key={name}
+                href={href}
+                className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+              >
+                <SvgIcon
+                  name={icon}
+                  className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-white'
+                />
+                <span className='ms-3'>{name}</span>
+              </Link>
+            ))}
           </div>
           <div className='pt-4 mt-4 space-y-1 font-medium border-t border-gray-200 dark:border-gray-700 flex flex-col'>
-           
-              {otherRoutes.map(({ name, href, icon }) => (
-                <Link
-                  href={href}
-                  className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
-                >
-                  {/* <SvgIcon
-                    name={icon}
-                    className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-white'
-                  /> */}
-                  <span className='ms-3'>{name}</span>
-                </Link>
-              ))}
-           
+            {otherRoutes.map(({ name, href, icon }) => (
+              <Link
+                key={name}
+                href={href}
+                className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+              >
+                <SvgIcon
+                  name={icon}
+                  className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-white'
+                />
+                <span className='ms-3'>{name}</span>
+              </Link>
+            ))}
           </div>
         </div>
       </aside>
