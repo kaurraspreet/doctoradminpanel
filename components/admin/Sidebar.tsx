18eae76d4ae5cf6ad1447353c953f76d199335ae
“Raspreet” <raspreet.dev@gmail.com>
2023-11-24 20:10:25 +0530

    remove icons

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -61,38 +61,38 @@ const Sidebar = () => {
         aria-label='Sidebar'
       >
         <div className='h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'>
-          <ul className='space-y-2 font-medium'>
-            <li>
+          <div className='flex flex-col space-y-1 font-medium'>
+            
               {mainRoutes.map(({ name, href, icon }) => (
                 <a
                   href={href}
                   className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
                 >
-                  <SvgIcon
+                  {/* <SvgIcon
                     name={icon}
                     className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-white'
-                  />
+                  /> */}
                   <span className='ms-3'>{name}</span>
                 </a>
               ))}
-            </li>
-          </ul>
-          <ul className='pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700'>
-            <li>
+          
+          </div>
+          <div className='pt-4 mt-4 space-y-1 font-medium border-t border-gray-200 dark:border-gray-700 flex flex-col'>
+           
               {otherRoutes.map(({ name, href, icon }) => (
                 <a
                   href={href}
                   className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
                 >
-                  <SvgIcon
+                  {/* <SvgIcon
                     name={icon}
                     className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-white'
-                  />
+                  /> */}
                   <span className='ms-3'>{name}</span>
                 </a>
               ))}
-            </li>
-          </ul>
+           
+          </div>
         </div>
       </aside>
     </>
